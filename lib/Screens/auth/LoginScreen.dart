909ec681b0dff48ca9e0c3237c412320aca78f86
Jack Conceprio <jackconceprio1@gmail.com>
2022-02-04 02:07:36 +0000

    Moved InputBox In to it's own file

--- lib/Screens/auth/LoginScreen.dart
+++ lib/Screens/auth/LoginScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // add google fonts
 import 'package:google_fonts/google_fonts.dart';
+import 'package:moviehut/widgets/InputBox.dart';
 class LoginScreen extends StatelessWidget {
   const LoginScreen({ Key? key }) : super(key: key);
 
@@ -65,38 +66,3 @@ class LoginScreen extends StatelessWidget {
   }
 }
 
-class InputBox extends StatelessWidget {
-  const InputBox({
-    Key? key,
-    required this.hintText,
-    required this.obscureText,
-    required this.keyboardType,
-  }) : super(key: key);
-
-  final String hintText;
-  final bool obscureText;
-  final TextInputType keyboardType;
-
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      obscureText: obscureText,
-      keyboardType: keyboardType,
-      style: GoogleFonts.rubik(color: Colors.black),
-
-      decoration: InputDecoration(        
-        border: const OutlineInputBorder(
-          gapPadding: 0,
-          borderSide: BorderSide.none,
-        
-          borderRadius: BorderRadius.all(Radius.circular(12.0)),
-        ),
-        filled: true,
-        fillColor: Colors.white,
-        // fillColor: Color(0xFFE0E0E0),
-        hintText: hintText,
-      ),
-    );
-  }
-}
